db6efce460fb5b863187a71675ddea554db17de1
pritammukherjee02 <you@example.com>
2021-12-21 16:29:29 +0530

    Enabled ScrollTrigger and made a few scroll-based animations (incomplete)

--- script.js
+++ script.js
@@ -5,3 +5,55 @@ burgermenu.addEventListener('click', () => {
     nav.classList.toggle('nav-active');
     burgermenu.classList.toggle('toggle');
 })
+
+// GSAP and ScrollTrigger
+
+gsap.registerPlugin(ScrollTrigger);
+
+let t1 = gsap.timeline({ defaults: {duration: 1} });
+
+gsap.from('.nav-container', {
+    y: -50,
+    opacity: 0,
+    duration: 0.5,
+});
+
+t1.from('.landing-branding', {
+    x: -50,
+    opacity: 0,
+    duration: 0.5,
+})
+.from('.quicklink-cards', {
+    y: 50,
+    opacity: 0,
+    duration: 0.5,
+})
+
+t1.from('.section-header', {
+    y: 50,
+    opacity: 0,
+    duration: 0.5,
+    scrollTrigger:{
+        trigger: '.section-header',
+        start: "top 80%",
+        scrub: true,
+    }
+})
+.from('.project-card1', {
+    x: -50,
+    opacity: 0.7,
+    duration: 0.5,
+    scrollTrigger:{
+        trigger: '.project-card1',
+        scrub: true,
+    }
+})
+.from('.project-card2', {
+    x: 50,
+    opacity: 0.7,
+    duration: 0.5,
+    scrollTrigger:{
+        trigger: '.project-card2',
+        scrub: true,
+    }
+})
