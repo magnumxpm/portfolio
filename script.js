ff2eb03e97bd9247d22db684584d9e2a75f80ecc
pritammukherjee02 <you@example.com>
2021-12-26 02:45:33 +0530

    Removed ScrollTrigger from the projects section

--- script.js
+++ script.js
@@ -10,48 +10,59 @@ burgermenu.addEventListener('click', () => {
 
 gsap.registerPlugin(ScrollTrigger);
 
-let t1 = gsap.timeline({ defaults: {duration: 1} });
+window.addEventListener('DOMContentLoaded', e => {
+    let t1 = gsap.timeline({ defaults: {duration: 1} });
 
-t1.from('.landing-branding', {
-    x: -50,
-    opacity: 0,
-    duration: 0.5,
-})
-.from('.quicklink-cards', {
-    y: 50,
-    opacity: 0,
-    duration: 0.5,
-})
-
-t1.from('.section-header', {
-    y: 50,
-    opacity: 0,
-    duration: 0.5,
-    scrollTrigger:{
-        trigger: '.section-header',
-        start: "top 80%",
-        end: "top 50%",
-        scrub: true,
-    }
-})
-.from('.project-card1', {
-    x: -50,
-    opacity: 0.7,
-    duration: 0.5,
-    scrollTrigger:{
-        trigger: '.project-card1',
-        end: "top 80%",
-        scrub: true,
-    }
-})
-.from('.project-card2', {
-    x: 50,
-    opacity: 0.7,
-    display: 'none',
-    duration: 0.5,
-    scrollTrigger:{
-        trigger: '.project-card2',
-        end: "top 80%",
-        scrub: true,
-    }
+    t1.from('.landing-branding', {
+        x: -50,
+        opacity: 0,
+        duration: 0.5,
+    })
+    .from('.quicklink-cards', {
+        y: 50,
+        opacity: 0,
+        duration: 0.5,
+    })
+    /*
+    .from('.section-header', {
+        y: 50,
+        opacity: 0,
+        duration: 0.5,
+        scrollTrigger:{
+            trigger: '.section-header',
+            start: "top 80%",
+            end: "top 50%",
+            scrub: true,
+        }
+    })
+    .from('.project-card1', {
+        x: -50,
+        opacity: 0,
+        display: 'none',
+        duration: 0.5,
+        scrollTrigger:{
+            trigger: '.project-card1',
+            end: "top 60%",
+        }
+    })
+    .from('.project-card2', {
+        x: 50,
+        opacity: 0,
+        display: 'none',
+        duration: 0.5,
+        scrollTrigger:{
+            trigger: '.project-card2',
+            end: "top 60%",
+        }
+    })
+    .from('.sub-section-header', {
+        x: -50,
+        opacity: 0,
+        duration: 0.5,
+        scrollTrigger: {
+            trigger: '.sub-section-header',
+            start: "top 80%"
+        }
+    })
+    */
 })
