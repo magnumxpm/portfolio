8cb210c03f495b70172ae3937494adda502fa1c3
pmukherjee <mukherjeepritam02@gmail.com>
2025-05-09 19:53:23 +0530

    added new experience

--- src/data/experienceData.ts
+++ src/data/experienceData.ts
@@ -7,4 +7,10 @@ export const experienceData: ExperienceItem[] = [
     description:
       "Freelance developed a full-stack application for managing accounts and tax files on prem, allowing multiple CAs to work collaboratively. Created an RBAC system to protect privacy integrity.",
   },
+  {
+    company: "LeadGPT",
+    tenure: "2024",
+    description:
+      "Worked as a Backend Development Engineer. Refactored older codebase into more a industry standard version. Fixed critical issues in Email delivery pipeline - a mission-critical component in the business. Designed and implememted detailed data-enabled statistics and analytics. Architected an Email reply detection system. Used RabbitMQ as a message queue to facilitate microservice-based design. Enabled CI/CD using Docker and Docker Swarm.",
+  },
 ];
